37af655aedd0f6d39fa20271e97bd4a35efd9c9c
mlkaufman14 <mlkaufman14@gmail.com>
2019-10-01 08:22:55 -0500

    added sign in, form input, and button components

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-sign-up.styles.scss'
+
+const SignInSignUp = () => (
+    <div className='signinsignup'>
+        <SignIn />
+    </div>  
+);
+
+export default SignInSignUp;
