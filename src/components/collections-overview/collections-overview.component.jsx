b761dc2e4194d1cdec45a9cbd5992b5716c46daf
mlkaufman14 <mlkaufman14@gmail.com>
2019-11-07 08:54:02 -0600

    created route for specific collection item, i.e. hats, jackets, etc.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 
-import CollectionPreview from '../collection-overview/collection-overview.component';
+import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import {selectCollections} from '../../redux/shop/shop.selector';
+import {selectCollections} from '../../redux/shop/shop.selectors';
 
-import './collection-overview.styles.scss';
+import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({collections}) => (
     <div className='collections-overview'>
---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import {selectCollection} from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({match}) => {
+    console.log(match);
+    return(
+        <div className='collection-page'>
+        <h2>Collection Page</h2>
+    </div>
+    );
+};
+
+const mapStateProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateProps )(CollectionPage);
