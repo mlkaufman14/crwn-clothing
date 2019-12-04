28cbd501569e4494df37cc45361743e48673d814
mlkaufman14 <mlkaufman14@gmail.com>
2019-12-04 08:26:36 -0600

    converted shop data array into an object

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,11 +7,18 @@ import {selectCollection} from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({match}) => {
-    console.log(match);
+const CollectionPage = ({collection}) => {
+    const {title, items} = collection;
     return(
         <div className='collection-page'>
-        <h2>Collection Page</h2>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            {
+                items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))
+            }
+        </div>
     </div>
     );
 };
@@ -20,4 +27,4 @@ const mapStateProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
 })
 
-export default connect(mapStateProps )(CollectionPage);
+export default connect(mapStateProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -11,5 +11,4 @@ const ShopPage = ({match}) => (
     </div>
 );
        
-
 export default ShopPage;
